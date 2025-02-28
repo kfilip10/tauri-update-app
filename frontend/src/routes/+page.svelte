cc08537e0587ab73b68867aab938871651a74192
kfilip10 <147560289+kfilip10@users.noreply.github.com>
2025-02-27 20:33:36 -0500

    updater start

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let message = 'Click the button to run Rust backend!';
+	import { Alert } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
+
+	async function callRust() {
+		const { invoke } = await import('@tauri-apps/api/core');
+		message = await invoke('greet', { name: 'User' });
+	}
+</script>
+
+<main>
+	<h1>Welcome to Tauri + SvelteKit</h1>
+	<p>{message}</p>
+	<Button on:click={callRust}>Call Rust</Button>
+	<div class="p-8">
+		<Alert>
+			<span class="font-medium">Info alert!</span>
+			Change a few things up and try submitting again.
+		</Alert>
+	</div>
+</main>
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,17 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::command;
+
+#[command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
+}
